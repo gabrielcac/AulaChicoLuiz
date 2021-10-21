cb51e3c555911440d94f622cee2a7d67d15deab7
Gabriel Coutinho <gabrielcac@gmail.com>
2021-10-21 18:47:31 -0300

    Commit Inicial

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+	public float speed;
+
+	void Update()
+    {
+		if(Input.GetKey(KeyCode.W))
+		{
+			transform.position += speed * Time.deltaTime * Vector3.forward;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position += speed * Time.deltaTime * Vector3.back;
+		}
+	}
+}
